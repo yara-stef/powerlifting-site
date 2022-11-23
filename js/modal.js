114e14c0c1fab8608f0966cc571a7369e1ea4d08
yara-stef <ymhladyak@gmail.com>
2022-11-23 18:27:29 +0000

    implemented one modal window

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.exercise');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelector('.show-modal');
